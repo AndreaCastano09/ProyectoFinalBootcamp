8f34fde8d63c2412e13083a780c81d2fa754c796
Andrea Castaño <andry292@gmail.com>
2025-05-19 11:59:47 -0500

    ProyectoFinal

---
+++ proyectottf-main/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import InfoEnergia from './components/InfoEnergia';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Estimador from './components/EstimadorRenovable';
+import Graficos from './components/Graficos';
+import Bienvenida from './components/Bienvenida';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Bienvenida />} /> {/* Nueva pantalla de bienvenida */}
+        <Route path="/principal" element={<InfoEnergia />} />
+        <Route path="/estimador" element={<Estimador />} />
+        <Route path="/graficos" element={<Graficos />} />
+      </Routes>
+    </Router>
+   
+  );
+}
+
+export default App;
---
+++ proyectottf-main/src/components/Bienvenida.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Bienvenida.css';
+import imagenFondo from '../aseets/pexels-pixabay-414837.jpg'; 
+
+
+const Bienvenida = () => {
+  const navigate = useNavigate();
+
+  const irAPrincipal = () => {
+    navigate('/principal');
+  };
+
+  return (
+    
+    <div className="bienvenida-container">
+  <img src={imagenFondo} alt="Fondo" className="fondo-imagen" />
+  
+  <div className="bienvenida-overlay">
+    <div className="bienvenida-content">
+      <h1 className="bienvenida-titulo">Transición Energética Justa</h1>
+      <p className="bienvenida-texto">Si cambias tu fuente de energía, cambiarás tu futuro</p>
+      <button className="boton-empezar" onClick={irAPrincipal}>EXPLORA TUS CONSUMOS</button>
+    </div>
+    <div className="bienvenida-footer">
+      <p className="bienvenida-autor">Desarrollado por Andrea Castaño</p>
+    </div>
+  </div>
+</div>
+  );
+};
+
+export default Bienvenida;
